a639a1afdbad1b22993b716af8a533d4d0c2b008
sergey.tyuterev <sergey.tyuterev@datadvance.net>
2016-11-15 18:04:50 +0300

    cosmetics

--- github_trending.py
+++ github_trending.py
@@ -3,7 +3,7 @@ import datetime
 import requests
 
 
-def get_trending_repositories(top_size=5, days_count=7):
+def get_trending_repositories(top_size, days_count):
     """
     Finds %top_size% repositories with the largest amount of stars
     created in the last %days_number% days.
@@ -15,47 +15,49 @@ def get_trending_repositories(top_size=5, days_count=7):
     payload = {'q': queue, 'sort': 'stars', 'order': 'desc'}
     base = 'https://api.github.com/search/repositories'
     response = requests.get(base, params=payload)
-    success_code = 200
-    if response.status_code != success_code:
+    try:
+        response.raise_for_status()
+    except requests.HTTPError:
         return None
     response_json = response.json()
     for repository in response_json['items'][:top_size]:
-        yield repository['owner']['login'], repository['name'], repository['html_url']
+        yield repository['owner']['login'],\
+            repository['name'],\
+            repository['html_url']
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    """
-    Finds repository called %repo_name% of user %repo_owner% and counts its open issues.
-    :returns open issues count.
-    """
     payload = {'state': 'open'}
     base = 'https://api.github.com/repos/{}/{}/issues'.format(repo_owner, repo_name)
     response = requests.get(base, params=payload)
-    success_code = 200
-    if response.status_code != success_code:
+    try:
+        response.raise_for_status()
+    except requests.HTTPError:
         return None
-    issues = list(filter(lambda x: 'pull_request' not in x, response.json()))
+    issues = list(filter(lambda issue: 'pull_request' not in issue, response.json()))
     return len(issues)
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--count', action='store', dest='count', default=5,
+    parser.add_argument('--count', action='store', type=int, dest='count', default=5,
                         help='the amount of repositories you want to retrieve')
-    parser.add_argument('--time', action='store', dest='time_period', default=7,
+    parser.add_argument('--time', action='store', type=int, dest='time_period', default=7,
                         help='repositories should not be older than this (in days)')
     return parser.parse_args()
 
 
 if __name__ == '__main__':
     arguments = parse_arguments()
-    repositories = get_trending_repositories(int(arguments.count),
-                                             int(arguments.time_period))
+    repositories = get_trending_repositories(arguments.count, arguments.time_period)
     if repositories is None:
         print('HTTP request failed, something went wrong. Try to try again later.')
         raise SystemExit
     for repository in repositories:
         repo_owner, repo_name, repo_link = repository
         reply_string = 'Repository "{}" of user "{}" has {} open issues. Link: {}'\
-            .format(repo_name, repo_owner, get_open_issues_amount(repo_owner, repo_name), repo_link)
+            .format(repo_name,
+                    repo_owner,
+                    get_open_issues_amount(repo_owner, repo_name),
+                    repo_link)
         print(reply_string)
